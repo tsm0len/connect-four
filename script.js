4abf67b653c6ca6db2ad42816857efc224269207
sm0L <122048823+tsm0len@users.noreply.github.com>
2024-10-26 13:05:43 +0200

    Update
    Visual moves

--- script.js
+++ script.js
@@ -1,6 +1,56 @@
-//7x6 wxh
+const combinations = [
+    [
+        [0, 0]
+        [0, 1],
+        [0, 2],
+        [0, 3],
+    ],
+]
+let board = [
+    ["gray", "gray", "gray", "gray","gray", "gray"], //rotated 90deg left
+    ["gray", "gray", "gray", "gray","gray", "gray"],
+    ["gray", "gray", "gray", "gray","gray", "gray"],
+    ["gray", "gray", "gray", "gray","gray", "gray"],
+    ["gray", "gray", "gray", "gray","gray", "gray"],
+    ["gray", "gray", "gray", "gray","gray", "gray"],
+    ["gray", "gray", "gray", "gray","gray", "gray"]
+];
 
-function test(x) {
-    alert(1)
-    document.getElementById("column"+x).style.backgroundColor = "white";
+let color = "red"
+function changeColor() {
+    if (color=="red") {
+        color = "yellow";
+    } else {
+        color = "red";
+    }
+}
+function refreshBoard() {
+    for (let c=0; c<7; c++) {
+        for (let r=0; r<6; ++r) {
+            let column = document.getElementById('column'+c);
+            let row = column.querySelector('#row'+r);
+            row.style.backgroundColor = board[c][r];
+        }
+    }
+}
+function checkForWin(placed) {
+    for (let c=0; c<4; c++) {
+        for (let r=0; r<3; ++r) {
+            if (board[c][r]==board[c+1][r+1] && board[c+1][r+1]==board[c][r]) {
+                return
+            }
+        }
+    }
+}
+function column(column) {
+	console.log("test");
+    for (let cell=7; cell>=0; cell--) {
+        if (board[column][cell]=="gray") {
+            board[column][cell] = color;
+            console.log(board);
+            changeColor();
+            break;
+        }
+    }
+    refreshBoard();
 }
